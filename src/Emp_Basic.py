b22bc9387aa20d85f9a697941ff3f51add9ca0ad
danuka dilshan <danukarathnayaka9@gmail.com>
2025-01-19 17:07:32 +0530

    second

---
+++ src/Emp_Basic.py
@@ -0,0 +1,79 @@
+
+from dotenv import load_dotenv
+import base64
+load_dotenv()
+import io
+import streamlit as st
+import os
+from PIL import Image
+import pdf2image 
+import google.generativeai as genai
+import pandas as pd
+
+
+df=pd.read_excel("C://Users//Danuka//Desktop//ATS//src//k.xlsx")
+
+df['Emp_id'] = df['Emp_id'].astype("string")
+
+
+e_df=df[['Emp_id','Emp Name', 'Age', 'Gender', 'Education Qualifications',
+         'Professional Qualifications With Years','List of Technical Skills',
+         'Programming & Software Skills', 'List of Soft Skills']]
+# employee_details = e_df[e_df['Emp_id'] == emp_id]
+# if not employee_details.empty:
+#     print(employee_details)
+# else:
+#     print(f"No employee found")
+
+
+
+
+
+# if submit4 is not None:
+#     employee_details = e_df[e_df['Emp_id'] == submit4]
+# else:
+#     print("Enter Your Employee ID")
+
+
+
+
+
+
+# if submit4:
+#     if submit4.strip():  # Ensure the Employee ID field is not empty or blank
+#         emp_details = e_df[e_df['Emp_id'] == submit4]
+#         if not emp_details.empty:
+#             name=emp_details['Emp Name'].values[0]
+#             age = emp_details['Age'].values[0]
+#             education = emp_details['Education Qualifications'].values[0]
+#             professional_qualifications = emp_details['Professional Qualifications With Years'].values[0]
+#             technical_skills = emp_details['List of Technical Skills'].values[0]
+#             programming_skills = emp_details['Programming & Software Skills'].values[0]
+#             soft_skills = emp_details['List of Soft Skills'].values[0]
+#             # Convert the filtered DataFrame to JSON format
+#             # employee_details_json = emp_details.to_json(orient='records', indent=4)
+#             input_prompt4 ="""
+# I will provide you with some information about a person, including their age, education qualifications, professional qualifications, years of experience, technical skills, programming skills, and soft skills. Based on this data, generate a professional description summarizing the person's background, expertise, and capabilities. The description should be formal and highlight their suitability for roles in their field.
+
+# Here is the information:
+# - Name:{name}
+# - Age: {age}
+# - Education Qualifications: {education}
+# - Professional Qualifications: {professional_qualifications}
+# - List of Technical Skills: {technical_skills}
+# - Programming Skills: {programming_skills}
+# - Soft Skills: {soft_skills}
+
+
+# Generate a concise but detailed professional summary based on the above information.
+
+# """
+#             response = get_gemini_response_Description(input_prompt4.format(details=emp_details), None)
+#             st.subheader("The response is:")
+#             st.write(response)
+#         else:
+#             # Handle the case where no employee is found
+#             st.warning(f"No employee found with ID {submit4}")
+#     else:
+#         st.warning("Insert the Employee ID")
+
--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 import base64
-load_dotenv()
 import io
 import streamlit as st
 import os
 from PIL import Image
 import pdf2image 
 import google.generativeai as genai
+from Emp_Basic import e_df
+
+load_dotenv()
 
 genai.configure(api_key=os.getenv("google_api_key"))
 def get_gemini_response(input,pdf_content,prompt):
@@ -39,18 +41,33 @@ def input_pdf_setup(uploaded_file):
 
 
 ## Streamlit App
-st.set_page_config(page_title="ATS Resume Expert")
-st.header("ATS tracking system")
-input_text=st.text_area("Job description: ",key="input")
-uploaded_file=st.file_uploader("Uploader your resume(pdf).........",type=["pdf"])
+st.set_page_config(page_title="TalentAligner")
+st.header("TalentAligner")
+col1, col2 = st.columns(2)
 
+with col1:
+    input_text = st.text_area("Job description: ", key="input")
+with col2:
+    upload_jd = st.file_uploader("Upload your JD (PDF)...", type=["pdf"])
+    if upload_jd is not None:
+        st.write("PDF uploaded Successfully") ## need to create 
+
+uploaded_file=st.file_uploader("Uploader your resume(pdf).........",type=["pdf"])
 
 if uploaded_file is not None:
     st.write("PDF uploaded Successfully")
-    
+
+
+col1,col2=st.columns(2)
+with col1:
+    submit4=st.text_input("Employee_ID : ")
+with col2:
+    st.text("If You are currently employee in this company You can Enter your Employee ID to generate basic description base on your resume")
+
 submit1=st.button("Tell me about the resume")
 submit2=st.button("how can i Imporove my skills")
 submit3=st.button("Presentage Match")
+
 
 input_prompt1 = """
  You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
@@ -58,13 +75,34 @@ input_prompt1 = """
  Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
 
+# input_prompt3 = """
+# You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science, Data analyst, Big data engineer ,DEVOPS
+# and ATS functionality, 
+# your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+# the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+# """
+
+
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science, Data analyst, Big data engineer ,DEVOPS
+You are an skilled profile and job description similarity messuring tool, scanner with a deep understanding of data science, Data analyst, Big data engineer ,DEVOPS
 and ATS functionality, 
-your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+your task is to evaluate the employee profile description against the provided job description. give me the percentage of match if the e,ployee profile matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.also you should provide 
+what similarity technique use to meassure the similarity.
 """
 
+input_prompt4 ="""
+I will provide you with some information about a person, including their age, education qualifications, professional qualifications, years of experience, technical skills, programming skills, and soft skills. Based on this data, generate a professional description summarizing the person's background, expertise, and capabilities. The description should be formal and highlight their suitability for roles in their field.
+
+Here is the information:
+- Name:{name}
+- Age: {age}
+- Education Qualifications: {education}
+- Professional Qualifications: {professional_qualifications}
+- List of Technical Skills: {technical_skills}
+- Programming Skills: {programming_skills}
+- Soft Skills: {soft_skills}
+Generate a concise but detailed professional summary based on the above information."""
 
 if submit1:
     if uploaded_file is not None:
@@ -77,12 +115,65 @@ if submit1:
         st.write("Please upload the resume")
 
 
-if submit3:
-    if uploaded_file is not None:
-        pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt3,pdf_content,input_text)
-        st.subheader("the response is ")
-        st.write(response)
+# if submit3:
+#     if uploaded_file is not None:
+#         pdf_content=input_pdf_setup(uploaded_file)
+#         response=get_gemini_response(input_prompt3,pdf_content,input_text)
+#         st.subheader("the response is ")
+#         st.write(response)
 
+#     else:
+#         st.write("Please upload the resume")
+
+
+
+# make employee description
+def get_gemini_response_Description(input,prompt):
+    model=genai.GenerativeModel('gemini-1.5-flash')
+    response1=model.generate_content([input,prompt])
+    return response1.text
+
+column_mapping = {
+    'Emp Name': 'name',
+    'Age': 'age',
+    'Education Qualifications': 'education',
+    'Professional Qualifications With Years': 'professional_qualifications',
+    'List of Technical Skills': 'technical_skills',
+    'Programming & Software Skills': 'programming_skills',
+    'List of Soft Skills': 'soft_skills',
+}
+
+
+if submit4:
+    if submit4 is not None:
+        emp_details = e_df[e_df['Emp_id'] == submit4]
+        emp_details = emp_details.rename(columns=column_mapping)
+        
+        if not emp_details.empty:
+            # Convert the filtered DataFrame to JSON format
+            employee_details_json = emp_details.to_json(orient='records', indent=4)
+        else:
+            st.write(f"No employee found with ID {submit4}")
+            employee_details_json = None
+
+        if employee_details_json:
+            response11 = get_gemini_response_Description(input_prompt4, employee_details_json)
+            st.subheader("The response is:")
+            st.write(response11)
     else:
-        st.write("Please upload the resume")
+        st.write("Insert the Employee ID")
+        
+
+# Pass response1 to the next block
+
+def get_gemini_response1(input,text,prompt):
+    model=genai.GenerativeModel('gemini-1.5-flash')
+    responsek=model.generate_content([input,text,prompt])
+    return responsek.text
+if submit3:
+    if response11:
+        response2 = get_gemini_response1(input_prompt3, response11, input_text)
+        st.subheader("The response is:")
+        st.write(response2)
+    else:
+        st.write("Please upload a valid response.")
